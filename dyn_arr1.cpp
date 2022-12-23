6791be6080e406cd672fd49a2379faf9128ffc2a
michalsuwala <flks126@protonmail.com>
2022-12-23 17:50:25 +0100

    15.12.2022

---
+++ dyn_arr1.cpp
@@ -0,0 +1,62 @@
+/*Napisz funkcję, która dostaje jako argumenty dwuwymiarową tablicę tablic o elementach typu int oraz jej wymiary, 
+i zmienia kolejność wierszy w tablicy w taki sposób, że wiersz pierwszy ma się znaleźć na miejscu drugiego, 
+wiersz drugi ma się znaleźć na miejscu trzeciego itd., 
+natomiast ostatni wiersz ma się znaleźć na miejscu pierwszego (przyjmujemy, 
+że elementy w wierszu są umieszczone w pamięci obok siebie).*/
+
+#include <iostream>
+
+void func(int **arr, int n, int m){
+    int *temp1 = new int[m];
+    int *temp2 = new int[m];
+    int *temp3 = new int[m];
+    for(int i = 0; i < m; i++){
+        temp1[i] = arr[n - 1][i];
+        temp2[i] = arr[0][i];
+    }
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < m; j++){
+            temp3[j] = arr[i][j];
+            arr[i][j] = temp2[j];
+            temp2[j] = temp3[j];
+        }
+    }
+    for(int i = 0; i < m; i++){
+        arr[0][i] = temp1[i];
+    }
+    delete[] temp1;
+    delete[] temp2;
+    delete[] temp3;
+}
+
+int main(){
+    int n = 4, m = 5;
+    int **arr = new int *[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = new int[m];
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            arr[i][j] = i * j;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+    func(arr, n, m);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    for(int i = 0; i < n; i++){
+        delete[] arr[i];
+    }
+    delete[] arr;
+    return 0;
+}
---
+++ dyn_arr2.cpp
@@ -0,0 +1,53 @@
+/*Napisz funkcję mul(), która mnoży dwie macierze reprezentowane przez dynamicznie alokowane tablice dwuwymiarowe 
+oraz funkcję print_matrix(), która wyświetla tak zdefiniowaną macierz. */
+
+#include <iostream>
+
+void mul(int **arr1, int **arr2, int n, int m, int o, int p){
+    if(m != o){
+        return; 
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < p; j++){
+            int temp = 0;
+            for(int k = 0, l = 0; k < o, l < m; k++, l++){
+                temp += arr1[i][l] * arr2[k][j];
+            }
+            
+            std::cout << temp << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main(){
+    int n = 4, m = 5, o = 5, p = 7;
+    int **arr1 = new int *[n];
+    for(int i = 0; i < n; i++){
+        arr1[i] = new int[m];
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            arr1[i][j] = 2;
+        }
+    }
+    int **arr2 = new int *[o];
+    for(int i = 0; i < o; i++){
+        arr2[i] = new int[p];
+    }
+    for(int i = 0; i < o; i++){
+        for(int j = 0; j < p; j++){
+            arr2[i][j] = 2;
+        }
+    }
+    mul(arr1, arr2, n, m, o, p);
+    for(int i = 0; i < n; i++){
+        delete[] arr1[i];
+    }
+    delete[] arr1;
+    for(int i = 0; i < o; i++){
+        delete[] arr2[i];
+    }
+    delete[] arr2;
+    return 0;
+}
