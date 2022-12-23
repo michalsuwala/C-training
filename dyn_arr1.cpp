e5431e98dfb380eb5eb91a76c707ec896114bb3d
michalsuwala <119967506+michalsuwala@users.noreply.github.com>
2022-12-23 17:53:55 +0100

    Update dyn_arr1.cpp

--- dyn_arr1.cpp
+++ dyn_arr1.cpp
@@ -39,7 +39,7 @@ int main(){
         for(int j = 0; j < m; j++){
             arr[i][j] = i * j;
         }
-    }
+    } 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             std::cout << arr[i][j] << " ";
